aab0ec43cabd111a0b5f475e6a0d3babc8ec9d97
Johann <>
2024-05-05 23:45:25 -0600

    Sintaxis JSX

--- src/App.js
+++ src/App.js
@@ -2,23 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let nombre = "Johann";
+  let auth = false;
+  let estaciones = ["Primavera", "Verano", "Otoño", "Invierno"]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editar <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <label htmlFor="nombre">Nombre</label>
+          <input type="text" id="nombre" placeholder={nombre} ></input>
+          <p>
+            {auth ? "El usuario esta logeado" : "El usuario no esta logeado"}
+          </p>
+          <p>
+            Editar <code>src/App.js</code> and save to reload.
+          </p>
+          <ul>
+            {estaciones.map((elem, index) => (
+            <li key={index}>{elem}</li>
+            ))}
+          </ul>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
